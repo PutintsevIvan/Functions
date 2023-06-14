b5ea1eb9c820196f7192a5b133876ebfd58ee81c
79283 <79283@LAPTOP-TT6N6EBU>
2023-06-14 12:09:34 +0300

    Done

--- Function/Main.cpp
+++ Function/Main.cpp
@@ -1,42 +1,47 @@
 #include<iostream>
 using namespace std;
-int Add(int a = 0, int b = 0, int c = 0);
+
+
+
+
+int Add(int a = 0, int b = 0, int c = 0);	//Прототип функции (Объявление функции - Function declaration)
 int Sub(int a, int b);
 int Mul(int a, int b);
-double  Div(int a, int b);
-void main()
+double Div(int a, int b);
 
+//Default arguments, Default parameters
+
+void main()
 {
 	setlocale(LC_ALL, "");
 	int a, b;
 	cout << "Введите два числа: "; cin >> a >> b;
-	int c = Add(a, b);   //Вызов (использование) функции (Function call)
+	int c = Add(a, b);	//Вызов (использование) функции (Function call)
 	cout << a << " + " << b << " = " << c << endl;
 	cout << a << " - " << b << " = " << Sub(a, b) << endl;
 	cout << a << " * " << b << " = " << Mul(a, b) << endl;
-	cout << a << " / " << b << " = " <<(double)Div(a, b) << endl;
+	cout << a << " / " << b << " = " << Div(a, b) << endl;
+	//cout << a << " ^ " << b << " = " << power(a, b) << endl;
 }
 
- 
-int Add(int a=0, int b=0,int c=0)   //Реализация функции(определение функции-Function definition)
+int Add(int a = 0, int b = 0, int c = 0)		//Реализация функции (Определение функции - Function definition)
 {
-	//Addition-сложение;
-	int sum = a + b+c;
+	//Addition - сложение
+	int sum = a + b + c;
 	return sum;
 }
 int Sub(int a, int b)
 {
-	//Subtraction-вычитание
+	//Subtraction - вычитание
 	return a - b;
-		
 }
 int Mul(int a, int b)
 {
-	//Multiplication-умножение
+	//Multiplication - умножение
 	return a * b;
 }
-double  Div(int a, int b)
+double Div(int a, int b)
 {
-	//Division-деление 
-	return(double) a / b;
+	//Division - деление
+	return (double)a / b;
 }
---
+++ Function2/Main.cpp
@@ -0,0 +1,119 @@
+#include<iostream>
+using namespace std;
+#define tab "\t"
+#define delimiter "\n--------------\n"
+void FillRand(int arr[], const int n);    //заполнение массива случайными числами 
+void FillRand2(double arr[], const int n); //заполнение массива случайными числами 
+void Print(const int arr[], const int n);   //вывод массива на экран
+void Print(const double arr[], const int n); //вывод массива на экран
+void Sort(int arr[], const int n); //сортировка массива
+int Sum(const int arr[], const int n); // сумма элементов массива
+double Sum(const double  arr[], const int n); //
+double Avg(const int arr[], const int n);     //средне-арифмитическое элементов массива
+int minValueIn(const int arr[], const int n); //минимальное значение массива
+int maxValueIn(const int arr[], const int n); //максимальное значение массива
+
+
+void main()
+{
+	setlocale(LC_ALL, "");
+	const int n = 5;
+	int arr[5];
+
+	FillRand(arr,n);
+	Print(arr, n);
+	cout << "Сумма элементов массива: " << Sum(arr, n) << endl;
+	cout << "Средне-арифмитическое элементов массива: " << Avg(arr, n) << endl;
+	cout << "Минимальное значение массива: " << minValueIn(arr, n) << endl;
+	cout << " Максимальное значение массива: " << maxValueIn(arr, n) << endl;
+	Sort(arr, n);
+	Print(arr, n);
+
+}
+
+void FillRand(double arr[], const int n)   //заполнение массива случайными числами
+{
+	for (int i = 0; i < n; i++)
+	{
+		arr[i] = rand() % 10;
+	}
+}
+void FillRand2(double arr[], const int n)    //заполняет массив случайными числами
+{
+	for (int i = 0; i < n; i++)
+	{
+		arr[i] = rand() % 10000;
+		arr[i] /= 100;
+	}
+}
+void Print(const int arr[], const int n) //вывод массива на экран
+{
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << tab;
+	}
+	cout << endl;
+}
+	void Print( const double arr[], const int n) //вывод массива на экран
+{
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << tab;
+	}
+	cout << endl;
+}
+	void Sort(int arr[], const int n) //сортировка массива
+	{
+		for (int i = 0; i < n; i++)
+		{
+			for (int j = 0; j < n + 1; j++)
+			{
+				if (arr[j] < arr[i])
+				{
+					int buffer = arr[i];
+						arr[i] = arr[j];
+					arr[i] = buffer;
+				}
+			}
+		}
+	}
+	int Sum(const int arr[], const int n)
+	{
+		int Sum = 0;
+		for (int i = 0; i < n; i++)
+		{
+			Sum += arr[i];
+		}
+		return Sum;
+	}
+	double Sum(const double arr[], const int n)
+	{
+		int Sum = 0;
+		for (int i = 0; i < n; i++)
+		{
+			Sum += arr[i];
+		}
+		return Sum;
+	}
+	double Avg(const int arr[], const int n)
+	{
+		return(double)Sum(arr, n) / n;
+	}
+	int minValueIn(const int arr[], const int n)
+	{
+		int min = arr[0];
+		for (int i = 0; i < n; i++)
+		{
+			if (arr[i] < min)min = arr[i];
+		}
+		return min;
+	}
+	int maxValueIn(const int arr[], const int n)
+	{
+		int max = arr[0];
+		for (int i = 0; i < n; i++)
+		{
+			if (arr[i] > max)max = arr[i];
+		}
+		return max;
+	}
