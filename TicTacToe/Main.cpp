d037a6150a1506313382dbfb94480a85a33a81d1
79283 <79283@LAPTOP-TT6N6EBU>
2023-06-29 11:51:40 +0300

    Done

--- TicTacToe/Main.cpp
+++ TicTacToe/Main.cpp
@@ -1,20 +1,25 @@
-#include<iostream>
+ï»¿#include<iostream>
 #include<conio.h>
 using namespace std;
-#define VERTICAL_OFFSET	 "\n\n\n\n\n"	
-#define HORIZONTAL_OFFSET "\t\t\t\t\t"
-void PrinField(char field[], const int n,char player);
-void move(char field[], const int n,char player);
+
+#define VERTICAL_OFFSET		"\n\n\n\n\n"
+#define HORIZONTAL_OFFSET	"\t\t\t\t\t"
+
+void PrintField(char field[], const int n, char player);
+void Move(char field[], const int n, char player);
+void Check(char field[], const int n, char player);
 
 void main()
 {
 	setlocale(LC_ALL, "");
 	const int n = 9;
-	char field[n] ="        "/*{ /*1,2,3,4,5,6,7,8,9*/;
-	PrinField(field, n,'x');
-	//move(field, n);
+	char field[n] = { /*1, 2, 3, 4, 5, 6, 7, 8, 9*/ };
+	PrintField(field, n, '0');
+	//Move(field, n);
+	cout << "Ð•Ñ‰Ðµ Ñ€Ð°Ð·Ð¾Ñ‡ÐµÐº?\nÃ„Ã  - Anykey\nÐÐµÑ‚ - Escape" << endl;
+	if (_getch() != 27) main();
 }
-void PrinField(char field[], const int n,char player)
+void PrintField(char field[], const int n, char player)
 {
 	system("CLS");
 	cout << VERTICAL_OFFSET;
@@ -29,27 +34,63 @@ void PrinField(char field[], const int n,char player)
 		}
 		cout << endl;
 		cout << HORIZONTAL_OFFSET;
-		if(i>0)cout << "--- --- ---";
+		if (i > 0)cout << "--- --- ---";
 		cout << endl;
 	}
-	move(field, n,player);
+	//Move(field, n, player);
+	Check(field, n, player);
 }
-void move(char field[], const int n,char player)
+void Move(char field[], const int n, char player)
 {
-char key;
-	//for (int i = 0; i < 256; i++)cout << i << (char)i << endl;
+	//for (int i = 0; i < 256; i++)cout << i << "\t" << (char)i << endl;
+	cout << "Player " << player << endl;
+	char key;
 	do
 	{
-		
 		key = _getch();
 		if (key == 27)return;
-		if (key < '1' || key >'9')cout << "Error: âûõîä çà ïðåäåëû èãðîâîãî ïîëÿ" << endl;
-		else if (field[key-49]!=0)cout << "Error: êëåòêà óæå çàíÿòà" << endl;
+		if (key < '1' || key > '9')	cout << "Error:Ð’Ñ‹Ñ…Ð¾Ð´ Ð·Ð° Ð¿Ñ€ÐµÐ´ÐµÐ»Ñ‹ Ð¸Ð³Ñ€Ð¾Ð²Ð¾Ð³Ð¾ Ð¿Ð¾Ð»Ñ" << endl;
+		else if (field[key - 49] != 0)	cout << "Error:ÐšÐ»ÐµÑ‚ÐºÐ° ÑƒÐ¶Ðµ Ð·Ð°Ð½ÑÑ‚Ð°" << endl;
+	} while (key < '1' || key > '9' || key[field - 49] != 0);
+	field[key - 49] = player;
+	/*if(player=='X')PrintField(field, n, '0');
+	else PrintField(field, n, 'X');*/
+	//PrintField(field, n, player == 'X' ? '0' : 'X');
+	PrintField(field, n, player);
+}
+void Check(char field[], const int n, char player)
+{
+	bool game_over = false;
+
+	if (field[0] == field[4] && field[4] == field[8] && field[4] != 0)game_over = true;
+	if (field[2] == field[4] && field[4] == field[6] && field[4] != 0)game_over = true;
+
+	if (field[0] == field[1] && field[1] == field[2] && field[0] != 0)game_over = true;
+	if (field[3] == field[4] && field[4] == field[5] && field[3] != 0)game_over = true;
+	if (field[6] == field[7] && field[7] == field[8] && field[6] != 0)game_over = true;
+
+	if (field[0] == field[3] && field[3] == field[6] && field[0] != 0)game_over = true;
+	if (field[1] == field[4] && field[4] == field[7] && field[1] != 0)game_over = true;
+	if (field[2] == field[5] && field[5] == field[8] && field[2] != 0)game_over = true;
+
+	bool draw = true;
+	for (int i = 0; i < n - 1; i++)
+	{
+		if (/*field[i] == field[i + 1] && */field[i] == 0)
+		{
+			draw = false;
+			break;
+		}
 	}
-	while (key < '1' || key >'9' ||key[field-49]!=0);
-	field[key - 49] ==player;
-     
-	/*if (player == 'x')PrinField(field, n, '0');
-	else PrinField(field,n,'x');*/
-	PrinField(field, n, player == 'x' ? '0' : 'x');
+	if (draw)
+	{
+		cout << "Draw" << endl;
+		return;
+	}
+	if (game_over)
+	{
+		cout << "Player " << player << " wins" << endl;
+		return;
+	}
+	Move(field, n, player == 'X' ? '0' : 'X');
 }
